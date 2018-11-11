75de7decf8e79b3bec9ab9aa3ed74e236763c266
R2D2isHuman <petre.cosminpetrut97@gmail.com>
2018-11-11 20:03:42 +0200

    almost PHome u take it from here

---
+++ app/src/main/java/com/example/aeroz/quizzapp/PHomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.aeroz.quizzapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phome);
+
+        TextView txtView_createQuiz = findViewById(R.id.txtView_createQuiz);
+        TextView txtView_publicQuizzes = findViewById(R.id.txtView_public);
+    }
+}
