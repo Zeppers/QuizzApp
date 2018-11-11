5994b437cf6e760cad74d82cd9edd2a810580eb2
PinkDolphin223 <andreea.niculae223@gmail.com>
2018-11-11 02:46:19 +0200

    No status bar and final commit for today

--- app/src/main/java/com/example/aeroz/quizzapp/OnboardingActivity.java
+++ app/src/main/java/com/example/aeroz/quizzapp/OnboardingActivity.java
@@ -1,13 +1,19 @@
 package com.example.aeroz.quizzapp;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class OnboardingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT > 19) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_onboarding);
     }
 }
