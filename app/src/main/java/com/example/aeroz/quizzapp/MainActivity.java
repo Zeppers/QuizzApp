febdac2865a384c11f4098b0ae178daacb3f654a
PinkDolphin223 <andreea.niculae223@gmail.com>
2018-11-11 02:30:56 +0200

    We got fonts, different colors and it's opening with Splashscreen

--- app/src/main/java/com/example/aeroz/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/aeroz/quizzapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.aeroz.quizzapp;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT > 19) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_main);
     }
-    
+
 }
