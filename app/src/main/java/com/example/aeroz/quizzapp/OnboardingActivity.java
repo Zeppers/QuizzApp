fe8a9fff66f07e50e01647c90f3fe69dc5307c0b
R2D2isHuman <petre.cosminpetrut97@gmail.com>
2018-12-08 21:25:25 +0200

    onboarding almost done(hardcoded headings and descriptions).

--- app/src/main/java/com/example/aeroz/quizzapp/OnboardingActivity.java
+++ app/src/main/java/com/example/aeroz/quizzapp/OnboardingActivity.java
@@ -6,32 +6,40 @@ import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.transition.Slide;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.aeroz.quizzapp.notActivities.SliderAdapter;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.w3c.dom.Text;
+
 
 public class OnboardingActivity extends AppCompatActivity {
 
     public ViewPager viewPager;
-    public LinearLayout linearLayout;
+    public LinearLayout mDotLayout;
     public SliderAdapter sliderAdapter;
+
+    public TextView[] mDots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
         viewPager = findViewById(R.id.viewPager);
-        linearLayout = findViewById(R.id.linearLayout);
+        mDotLayout = findViewById(R.id.linearLayout);
 
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
+        addDotsIndicator(0);
+        viewPager.addOnPageChangeListener(viewListener);
 
         Button button = findViewById(R.id.btn_onboarding_continue);
         button.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +48,37 @@ public class OnboardingActivity extends AppCompatActivity {
                 startActivity(new Intent(OnboardingActivity.this,SigninActivity.class));
             }
         });
+
+    }
+    public void addDotsIndicator(int position){
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+        for(int i = 0;i<mDots.length;i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setTextSize(30);
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextColor(getResources().getColor(R.color.black));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length>0){
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
+        }
     }
 
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+        }
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 }
