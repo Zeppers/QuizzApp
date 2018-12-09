aeb6cfdfdae7ee5be4690294fbc3aa22c32663d9
PinkDolphin223 <andreea.niculae223@gmail.com>
2018-12-09 23:40:54 +0200

    demoquiz+lista

---
+++ app/src/main/java/com/example/aeroz/quizzapp/DemoQuizActivity.java
@@ -0,0 +1,13 @@
+package com.example.aeroz.quizzapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_quiz);
+    }
+}
